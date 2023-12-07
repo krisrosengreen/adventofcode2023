bece8d882e34f205b102ff240f234c8df40adfe6
krisrosengreen <rosengreenpedersen@proton.me>
2023-12-07 11:32:05 +0100

    day5 B too slow, changing code

--- day5/src/main.cpp
+++ day5/src/main.cpp
@@ -51,7 +51,7 @@ unsigned long int get_destination_value(string line, unsigned long int sec_sourc
     unsigned long int diff = sec_source_val - srs;
     unsigned long int destination = drs + diff;
 
-    cout << "from " << sec_source_val << " to " << destination << endl;
+    // cout << "from " << sec_source_val << " to " << destination << endl;
 
     return destination;
 }
@@ -90,18 +90,60 @@ unsigned long int part_A(string file_input) {
     vec_seed_dest ans;
 
     for (unsigned long int seed : seeds) {
-        cout << "[SEED] " << seed << endl;
+        // cout << "[SEED] " << seed << endl;
         unsigned long int dest = rec_dim_conv(dim_conv, 0, seed);
         ans.push_back(make_pair(seed, dest));
     }
 
-    cout << "Answers: " << endl;
+    // cout << "Answers: " << endl;
 
     unsigned long int seed = 1000000000;
     unsigned long int loc = 10000000000;
 
     for (auto pair : ans) {
-        cout << pair.first << " -> " << pair.second << endl;
+        // cout << pair.first << " -> " << pair.second << endl;
+        
+        if (pair.second < loc) {
+            loc = pair.second;
+            seed = pair.first;
+        }
+    }
+
+    return loc;
+}
+
+unsigned long int part_B(string file_input) {
+    string content = utils::read_input(file_input);
+    auto double_nl_split = utils::split(content, "\n\n");
+    rmatch regm = utils::reg_match(double_nl_split[0], "[0-9]+");
+    vector<string> seeds_str = regm.strs;
+    vector<unsigned long int> seeds = str_map_ulint(seeds_str);
+
+    // Remove first section containing seeds from double_nl_split vector
+    double_nl_split.erase(double_nl_split.begin());
+
+    section_lines dim_conv = parse_map(double_nl_split);
+
+    vec_seed_dest ans;
+
+    for (int i = 0; i < seeds.size(); i+=2) {
+        unsigned long int seed_start = seeds[i];
+
+        for (unsigned long int next = 0; next < seeds[i+1] - 1; next++) {
+            unsigned long int seed = seed_start + next;
+            // cout << "[SEED] " << seed << endl;
+            unsigned long int dest = rec_dim_conv(dim_conv, 0, seed);
+            ans.push_back(make_pair(seed, dest));
+        }
+    }
+
+    // cout << "Answers: " << endl;
+
+    unsigned long int seed = 1000000000;
+    unsigned long int loc = 10000000000;
+
+    for (auto pair : ans) {
+        // cout << pair.first << " -> " << pair.second << endl;
         
         if (pair.second < loc) {
             loc = pair.second;
@@ -117,8 +159,17 @@ void test_A() {
     cout << "(TEST A) " << ans << endl;
 }
 
+void test_B() {
+    unsigned long int ans = part_B(test);
+    cout << "(TEST B) " << ans << endl;
+}
+
 int main() {
     test_A();
+    test_B();
     unsigned long int ans = part_A(input);
     cout << "(A) " << ans << endl;
+
+    unsigned long int ans_B = part_B(input);
+    cout << "(B) " << ans_B << endl;
 }
